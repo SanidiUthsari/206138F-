70681bb486156be6aa0839b176cc8d7a3b7d4370
SanidiUthsari <sanidiuthsari@gmail.com>
2023-10-13 20:31:00 +0530

    upgrade the code

--- python2.py
+++ python2.py
@@ -1,10 +1,33 @@
-candidate = "Sanidi"
 
-print("Welcome!)
+candidates = ["A", "B", "C", "D", "E"]
 
-vote = input("Do you want to vote for sanidi? ): ").upper()
 
-if vote == "YES":
-    print("Thank you for voting for Sanidi!")
-else:
-    print("Your vote has not been recorded. ")
+candidate_votes = {candidate: 0 for candidate in candidates}
+
+print("Welcome to the Basic Voting System!")
+
+
+for candidate in candidates:
+    print(candidate)
+
+
+vote = input("Enter the name of the candidate you want to vote for (or 'exit' to end voting): ").strip()
+
+while vote.lower() != "exit":
+    if vote in candidates:
+        candidate_votes[vote] += 1
+        print(f"Thank you for voting for {vote}!\n")
+    else:
+        print("Invalid candidate name. try again.")
+
+   
+    vote = input("Enter the name of the candidate you want to vote for (or 'exit' to end voting): ").strip()
+
+print("Voting has ended. Here are the results:")
+
+for candidate, votes in candidate_votes.items():
+    print(f"{candidate}: {votes} votes")
+
+
+winner = max(candidate_votes, key=candidate_votes.get)
+print("The winner is {winner} with {candidate_votes[winner]} votes!")
