8dd433c1db59e56052a9adc3f30203e426488d3d
SanidiUthsari <sanidiuthari@gmail.com>
2023-10-13 09:10:39 +0530

    Upgrade the code

--- 206138F_L.S.Uthsari.py
+++ 206138F_L.S.Uthsari.py
@@ -1,5 +1,17 @@
-age = int(input("Enter Your age:"))
-if age >= 18:
-    print("you can vote")
+Minimum_Voting_age = 18
+age = int(input("Enter your age:"))
+
+if age >=   Minimum_Voting_age :
+    print("You can vote")
+    vote = input("do you prefer to vote?").upper()
+    if vote == "YES":
+        print("Great! you can elect.")
+    elif vote == "NO":
+        print(" may be next time :)")
+    else:
+        print("invalid input :(")
 else:
-    print("you cannot vote")
+    print("You cannot vote, come back when you are 18 :)")
+
+
+
