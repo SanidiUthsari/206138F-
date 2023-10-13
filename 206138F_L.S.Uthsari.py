3fdc6b3ec7fa48a6aac5f8b17f02dc7a626276df
SanidiUthsari <sanidiuthari@gmail.com>
2023-10-13 11:33:07 +0530

    add an exception

--- 206138F_L.S.Uthsari.py
+++ 206138F_L.S.Uthsari.py
@@ -1,21 +1,30 @@
+
 print("welcome to the voting system :)")
 name = input("Enter your name:")
 print("Hello",name)
 
-Minimum_Voting_age = 18
-age = int(input("Enter your age:"))
 
-if age >=   Minimum_Voting_age :
-    print("You can vote")
-    vote = input("do you prefer to vote?").upper()
-    if vote == "YES":
-        print("Great! you can elect.")
-    elif vote == "NO":
-        print(" may be next time :)")
+minimum_Voting_age = 18 
+
+def check_age(age):
+    if age >= minimum_Voting_age :
+        print("You are eligible to vote!")
+        vote = input("Do you want to vote?" ).upper()
+        if vote == "YES":
+            print("Great! You can cast your vote.")
+        elif vote == "NO":
+            print("Okay, maybe next time!")
+        else:
+            print("Invalid input.")
     else:
-        print("invalid input :(")
-else:
-    print("You cannot vote, come back when you are 18 :)")
+        print("You cannot vote, Come back when you're 18 :).")
+try:
+    age = int(input("Enter your age: "))
+    check_age(age)
+except ValueError:
+    print("Invalid input. Please enter a valid age.")
+
+print("Thank you!")
 
 
 
