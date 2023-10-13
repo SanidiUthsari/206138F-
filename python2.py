29cd0db53a7e1943b389347d27b145bac0fdc268
SanidiUthsari <sanidiuthsari@gmail.com>
2023-10-13 20:18:44 +0530

    Adding new python file

---
+++ python2.py
@@ -0,0 +1,10 @@
+candidate = "Sanidi"
+
+print("Welcome!)
+
+vote = input("Do you want to vote for sanidi? ): ").upper()
+
+if vote == "YES":
+    print("Thank you for voting for Sanidi!")
+else:
+    print("Your vote has not been recorded. ")
